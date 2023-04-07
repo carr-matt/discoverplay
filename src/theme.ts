1a0c6b65c95e50206df500c2a76e0204e4c7fb2d
Matt <carr.matthew.lee@gmail.com>
2023-04-06 19:11:28 -0500

    added purple color palette, commented out

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,25 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-}
+};
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  // colors: {
+  //   gray: { // Purple color palette
+  //     50: '#FAF5FF',
+  //     100: '#E9D8FD',
+  //     200: '#D6BCFA',
+  //     300: '#B794F4',
+  //     400: '#9F7AEA',
+  //     500: '#805AD5',
+  //     600: '#6B46C1',
+  //     700: '#553C9A',
+  //     800: '#44337A',
+  //     900: '#322659',
+  //   },
+  // },
+});
 
-export default theme
+export default theme;
